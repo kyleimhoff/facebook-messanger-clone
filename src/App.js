6fcb6b8e32061e410d6c0d9a93f40f1327eabe47
kyleimhoff <kyleimhoff@DESKTOP-22RNIN5.localdomain>
2021-05-17 23:43:37 -0400

    setup input and state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
 function App() {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  console.log(messages);
+
+  const sendMessage = (event) => {
+    //logic to send msg
+    setMessages([...messages, input]);
+    setInput('');
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Hello World</h1>
+
+      <input value={input} onChange={event => setInput(event.target.value)}/>
+      <button onClick={sendMessage}>Send Message</button>
+      
+      {/* messages */}
+
     </div>
   );
 }
